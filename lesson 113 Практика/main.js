9663f67766f14079483e598310ab7ec9ec285c1c
TamyAmbal <66521690+TamyAmbal@users.noreply.github.com>
2025-10-03 20:44:23 +0300

    Update main.js

--- lesson 113 Практика/main.js
+++ lesson 113 Практика/main.js
@@ -17,7 +17,7 @@ btn.addEventListener('click', openModal)
 modal.addEventListener('click', event => {
     const target = event.target
 
-    if (target && target.classList.contains('modal') || target.classList.contains('.modal__close-btn')) {
+    if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
         closeModal()
     }
 })
@@ -26,4 +26,5 @@ document.addEventListener('keydown', event => {
     if(event.code === 'Escape' &&  modal.classList.contains('modal--open')) {
         closeModal()
     }
+
 } )
